d8a922e05dc68cf966ca5898f1a28fcd1591a307
Mike Shahindoust <gobias101@gmail.com>
2020-08-28 23:04:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,7 +20,7 @@ class Triangle
   end
   
   def valid
-    triangles = [(a + b > c), (a + c > b), (b + c > a)] && (a > 0 || b > 0 || c > 0)
+    triangles = [(a + b > c), (a + c > b), (b + c > a)] && [a > 0  && b > 0 && c > 0]
     raise TraingleError if triangles.include?(false)
     
   end
